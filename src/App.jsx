6eeee710109337e1230dd322ce27eebc212daa7b
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-11-01 22:14:41 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 function App() {
     const [message, setMessage] = useState('');
 
+
     useEffect(() => {
         fetch("http://localhost:9090/hello")
             .then(response => {
