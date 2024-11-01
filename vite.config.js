fc6d5e5fa69ef6337d3c7ff386b6a0c85a39c9d6
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-11-01 15:59:52 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/zindriana/',
+  base: '/https://zindriana.github.io/react-to-java/',
 })
