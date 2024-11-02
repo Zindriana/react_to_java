35d6cb5694c8b3163ca1377e8c64bc6332089e3c
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-11-02 16:03:39 +0100

    added input fields and a fetch to create a new character

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ function App() {
     const [message, setMessage] = useState('');
     const [dieValue, setDieValue] = useState(0);
     const [amountOfDiceInput, setAmountOfDiceInput] = useState(1);
+    const [newCharacterName, setNewCharacterName] = useState('');
+    const [newMental, setNewMental] = useState('');
+    const [newPhysical, setNewPhysical] = useState('');
+    const [newSocial, setNewSocial] = useState('');
+    const [newSpirit, setNewSpirit] = useState('');
 
 
     useEffect(() => {
@@ -15,7 +20,6 @@ function App() {
                 return response.text();
             })
             .then(data => {
-                console.log('Data received:', data); // Lägg till denna rad
                 setMessage(data);
             })
             .catch(error => console.error('Error:', error));
@@ -35,6 +39,32 @@ function App() {
             .catch(error => console.error('Error:', error));
     }
 
+    function createCharacter(){
+        fetch('http://localhost:9090/newcharacter', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: newCharacterName,
+                mental: newMental,
+                physical: newPhysical,
+                social: newSocial,
+                spirit: newSpirit,
+            }),
+        })
+
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.text();
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+
     return (
         <div className="App">
             <h1>{message}</h1>
@@ -42,6 +72,25 @@ function App() {
             <input className="amountOfDiceInput" type="number" placeholder="1" min="1"
                    onChange={(event) => setAmountOfDiceInput(Number(event.target.value))}/>
             <button className="rollDieButton" onClick={rollDie}>Roll die</button>
+            <br/>
+            <h3>Create a new character</h3>
+            <h4>Character name</h4>
+            <input className="newCharacterName" type="text" placeholder="Name"
+            onChange={(event) => setNewCharacterName(event.target.value)}/>
+            <h4>Character mental value</h4>
+            <input className="newMentalValue" type="number" placeholder="1" min="1"
+            onChange={(event) => setNewMental(event.target.value)}/>
+            <h4>Character physical value</h4>
+            <input className="newPhysicalValue" type="number" placeholder="1" min="1"
+            onChange={(event) => setNewPhysical(event.target.value)}/>
+            <h4>Character social value</h4>
+            <input className="newSocialValue" type="number" placeholder="1" min="1"
+            onChange={(event) => setNewSocial(event.target.value)}/>
+            <h4>Character spirit value</h4>
+            <input className="newSpiritValue" type="number" placeholder="1" min="1"
+            onChange={(event) => setNewSpirit(event.target.value)}/>
+            <br/>
+            <button className="createNewCharacterBtn" onClick={createCharacter}>Create character</button>
         </div>
     );
 }
