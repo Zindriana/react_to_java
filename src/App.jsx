9e06ba9a5bc997cb14bd827b630179df342c2895
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-11-02 14:07:44 +0100

    added a button to call for rolldie() in the backend

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 function App() {
     const [message, setMessage] = useState('');
+    const [dieValue, setDieValue] = useState(0);
 
 
     useEffect(() => {
@@ -19,9 +20,25 @@ function App() {
             .catch(error => console.error('Error:', error));
     }, []);
 
+    function rollDie() {
+        fetch("http://localhost:9090/rolldie")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.text();
+            })
+            .then(data => {
+                setDieValue(Number(data));
+            })
+            .catch(error => console.error('Error:', error));
+    }
+
     return (
         <div className="App">
             <h1>{message}</h1>
+            <h2>Die Roll = {dieValue}</h2>
+            <button className="rollDieButton" onClick={rollDie}>Roll die</button>
         </div>
     );
 }
