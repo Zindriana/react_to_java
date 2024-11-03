a1ac2c39f9ed61c39829238fe9404a11944b79e2
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-11-03 14:09:35 +0100

    updated the local tunnel

--- src/components/NewCharacter.jsx
+++ src/components/NewCharacter.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { API_BASE_URL } from './config';
+import { API_BASE_URL } from '../config';
 
 function Create() {
     const [newCharacterName, setNewCharacterName] = useState('');
