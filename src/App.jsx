b213629442149cc1146fe7b0e7a0589c674ee45a
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-11-04 15:38:07 +0100

    added links and routes to Scroll

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { HashRouter, Route, Routes, Link } from 'react-router-dom';
 import CharacterList from "./components/CharacterList.jsx";
 import NewCharacter from "./components/NewCharacter.jsx";
+import ScrollList from "./components/ScrollList.jsx";
+import NewScroll from "./components/NewScroll.jsx";
 
 function App() {
     const [message, setMessage] = useState('');
@@ -50,11 +52,15 @@ function App() {
                     <Link to="/">Home</Link> |
                     <Link to="/characterlist">Character List</Link> |
                     <Link to="/newcharacter">New Character</Link>
+                    <Link to="/newcharacter">Scroll List</Link>
+                    <Link to="/newcharacter">New Scroll</Link>
                 </nav>
                 <Routes>
                     <Route path="/" element={<div>Home</div>}/>
                     <Route path="/characterlist" element={<CharacterList/>}/>
                     <Route path="/newcharacter" element={<NewCharacter/>}/>
+                    <Route path="/scrolllist" element={<ScrollList/>}/>
+                    <Route path="/newscroll" element={<NewScroll/>}/>
                 </Routes>
             </div>
         </HashRouter>
